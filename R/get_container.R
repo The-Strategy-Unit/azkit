4805c201b796333b02cb1f62b096deb72e043f3c
Fran Barton <francis.barton@nhs.net>
2025-08-15 14:27:03 +0100

    ✅ Add checks to get_container()

--- R/get_container.R
+++ R/get_container.R
@@ -1,14 +1,31 @@
 #' Get Azure storage container
 #'
 #' `r lifecycle::badge("experimental")`
-#' @param container_name string: name of the container
+#' Use `list_container_names()` to see a list of available containers
+#'
+#' @param container_name Name of the container as a string. NULL by default,
+#'  which means the function will look instead for a container name stored in
+#'  the environment variable "AZ_STORAGE_CONTAINER"
 #' @param ... arguments to be passed through to `get_auth_token()`
+#' @returns An Azure blob container (list object of class "blob_container")
 #' @export
-get_container <- function(container_name, ...) {
-  endpoint_uri <- Sys.getenv("AZ_STORAGE_EP")
+get_container <- function(container_name = NULL, ...) {
+  container_envvar_name <- "AZ_STORAGE_CONTAINER"
+  cst_msg1 <- cst_error_msg("{.var container_name} must be a string")
+  cst_msg2 <- cst_error_msg("{.envvar {container_envvar_name}} is not set")
+  c_name <- (container_name %||% Sys.getenv(container_envvar_name, NA)) |>
+    check_scalar_type("character", cst_msg1) |>
+    check_scalar_type("string", cst_msg2)
   token <- get_auth_token(...)
   endpoint <- get_default_endpoint(token)
   container_names <- list_container_names(token)
+  not_found_msg <- cv_error_msg("Container {.val {c_name}} not found")
+  c_name |>
+    check_vec(\(x) x %in% container_names, not_found_msg) |>
+    AzureStor::blob_container(endpoint = endpoint)
+}
+
+
 #' Return an Azure "blob_endpoint"
 #'
 #' @param token An Azure authentication token
