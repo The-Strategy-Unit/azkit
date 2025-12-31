73fb6bb30e85c986f0a1b1729b00a0ae227be04f
Fran Barton <francis.barton@nhs.net>
2025-12-31 13:17:04 +0000

    🔨 Update get_container function

--- R/get_container.R
+++ R/get_container.R
@@ -1,34 +1,48 @@
 #' Get Azure storage container
 #'
-#' The environment variable "AZ_STORAGE_EP" must be set. This provides the URL
+#' The environment variable "AZ_STORAGE_EP" should be set. This provides the URL
 #'  for the default Azure storage endpoint.
 #' Use [list_container_names] to get a list of available container names.
 #'
+#' @param token An Azure authentication token. If left as `NULL`, a token
+#'  returned by [get_auth_token] will be used
 #' @param container_name Name of the container as a string. `NULL` by default,
 #'  which means the function will look instead for a container name stored in
 #'  the environment variable "AZ_CONTAINER"
-#' @param ... arguments to be passed through to [get_auth_token]
+#' @param endpoint_url An Azure endpoint URL. If left as `NULL`, the default,
+#'  the value of the environment variable "AZ_STORAGE_EP" will be used
+#' @param ... arguments to be passed through to [get_auth_token], if a token is
+#'  not already supplied
 #' @returns An Azure blob container (list object of class "blob_container")
 #' @export
-get_container <- function(container_name = NULL, ...) {
+get_container <- function(
+  token = NULL,
+  container_name = NULL,
+  endpoint_url = NULL,
+  ...
+) {
   msg <- glue::glue(
     "{.var container_name} is empty. ",
     "Did you forget to set an environment variable?"
   )
-  possibly_list_cont_names <- \(...) purrr::possibly(list_container_names)(...)
   cont_nm <- check_nzchar(container_name, msg) %||% check_envvar("AZ_CONTAINER")
-  token <- get_auth_token(...)
-  endpoint <- get_default_endpoint(token)
+  token <- token %||% get_auth_token(...)
+  endpoint <- get_azure_endpoint(token, endpoint_url)
 
   # list_container_names() fails when run on Connect deployment, so work around:
+  possibly_list_cont_names <- \(...) purrr::possibly(list_container_names)(...)
   container_names <- possibly_list_cont_names(token)
 
   if (is.null(container_names)) {
     if (rlang::is_interactive()) {
-      cli::cli_alert_info("Unable to check that container name exists")
+      cli::cli_alert_info("Unable to check that the container name exists")
+      cli::cli_alert_info("Attempting to return container {.val {cont_nm}}")
     }
     AzureStor::blob_container(endpoint, cont_nm)
   } else {
+    if (rlang::is_interactive()) {
+      cli::cli_alert_info("Attempting to return container {.val {cont_nm}}")
+    }
     not_found_msg <- ct_error_msg("Container {.val {cont_nm}} not found")
     cont_nm |>
       check_that(\(x) x %in% container_names, not_found_msg) |>
@@ -37,18 +51,15 @@ get_container <- function(container_name = NULL, ...) {
 }
 
 
-#' Return a list of container names that are found at the default endpoint
+#' Return a list of container names that are found at the endpoint
 #'
 #' @inheritParams get_container
-#' @inheritParams get_default_endpoint
 #' @returns A character vector of all container names found
 #' @export
-list_container_names <- function(token = NULL, ...) {
+list_container_names <- function(token = NULL, endpoint_url = NULL, ...) {
   token <- token %||% get_auth_token(...)
-  endpoint <- get_default_endpoint(token)
-  lcn <- "list_container_names" # nolint
-  container_list <- AzureStor::list_blob_containers(endpoint) |>
-    rlang::try_fetch(error = \(e) cli::cli_abort("Error in {.fn {lcn}}: {e}"))
+  endpoint <- get_azure_endpoint(token, endpoint_url)
+  container_list <- AzureStor::list_blob_containers(endpoint)
   stopifnot("no containers found" = length(container_list) >= 1L)
   names(container_list)
 }
@@ -56,12 +67,16 @@ list_container_names <- function(token = NULL, ...) {
 
 #' Return an Azure "blob_endpoint"
 #'
-#' @param token An Azure authentication token
+#' This function will return the endpoint specified in the environment variable
+#' "AZ_STORAGE_EP" by default
+#'
+#' @inheritParams get_container
 #' @returns An Azure blob endpoint (object of class "blob_endpoint")
 #' @keywords internal
-get_default_endpoint <- function(token) {
-  check_envvar("AZ_STORAGE_EP") |>
-    AzureStor::blob_endpoint(token = token)
+get_azure_endpoint <- function(token = NULL, endpoint_url = NULL, ...) {
+  token <- token %||% get_auth_token(...)
+  endpoint_url <- endpoint_url %||% check_envvar("AZ_STORAGE_EP")
+  AzureStor::blob_endpoint(endpoint_url, token = token)
 }
 
 
