433f561aa84d9e8f931381dad6038e3de5ecf294
Tom Jemmett <thomas.jemmett@nhs.net>
2026-05-12 09:08:31 +0100

    adds odata filter/select/top filters

--- R/read_azure_table.R
+++ R/read_azure_table.R
@@ -3,12 +3,18 @@
 #' @param table_name Name of the table to be read.
 #' @param table_endpoint An Azure table endpoint URL.
 #' @inheritParams get_container
+#' @param filter An OData filter string to filter the results.
+#' @param select An OData select string to specify which properties to return.
+#' @param top An integer specifying the maximum number of records to return.
 #' @returns A tibble
 #' @export
 read_azure_table <- function(
   table_name,
   table_endpoint = Sys.getenv("AZ_TABLE_EP"),
-  token = get_auth_token()
+  token = get_auth_token(),
+  filter = NULL,
+  select = NULL,
+  top = NULL
 ) {
   base_req <- httr2::request(table_endpoint) |>
     httr2::req_url_path_append(table_name) |>
@@ -17,6 +23,16 @@ read_azure_table <- function(
       "x-ms-version" = "2025-11-05",
       "Accept" = "application/json;odata=nometadata"
     )
+
+  if (!is.null(filter)) {
+    base_req <- httr2::req_url_query(base_req, `$filter` = filter)
+  }
+  if (!is.null(select)) {
+    base_req <- httr2::req_url_query(base_req, `$select` = select)
+  }
+  if (!is.null(top)) {
+    base_req <- httr2::req_url_query(base_req, `$top` = top)
+  }
 
   responses <- httr2::req_perform_iterative(
     req = base_req,
